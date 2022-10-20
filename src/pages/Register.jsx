198fd581d90ecd57a5e35248f5864743f47d6d96
Int-Angel <Padilla.angel88@gmail.com>
2022-10-20 17:20:19 -0500

    Register page

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,16 +1,21 @@
 import React from 'react'
+import AddImage from '../img/addAvatar.png'
 
 const Register = () => {
   return (
     <div className='formContainer'>
         <div className='formWrapper'>
-            <span className='logo'>Real Time Chat</span>
+            <span className='logo'>Messenger</span>
             <span className='title'>Register</span>
             <form>
                 <input type="text" placeholder='username'/>
                 <input type="email" placeholder='email'/>
                 <input type="password" placeholder='password'/>
-                <input type="file"/>
+                <input style={{display:"none"}} type="file" id="file"/>
+                <label htmlFor="file">
+                  <img src={AddImage} alt="add image"/>
+                  <span>Add an image</span>
+                </label>
                 <button>Sign Up</button>
             </form>
             <p>You do have an account? Login</p>
