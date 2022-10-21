6446cd9a78988aeca8306949b7c6badc1c59f51f
Int-Angel <Padilla.angel88@gmail.com>
2022-10-20 19:12:24 -0500

    Chat info

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import CamImg from '../img/cam.png'
+import AddImg from '../img/add.png'
+import MoreImg from '../img/more.png'
 
 function Chat() {
   return (
-    <div className='chat'>Chat</div>
+    <div className='chat'>
+      <div className="chatInfo">
+        <span>Name</span>
+        <div className="chatIcons">
+          <img src={CamImg} alt=""/>
+          <img src={AddImg} alt=""/>
+          <img src={MoreImg} alt=""/>
+        </div>
+      </div>
+    </div>
   )
 }
 
